8faeab8a803beb1bee4ea81b306d5cfb42dc5634
SoftwareDevelopHER <kenyatta.martin1992@gmail.com>
2022-06-02 17:20:35 -0500

    Crated React Restaurant-app

---
+++ Apps.js
@@ -0,0 +1,27 @@
+import Register from './Register';
+import Login from './Login';
+import React, {Component} from 'react'
+
+function App() {
+
+
+    return (
+        <main className="App">
+            <Login />
+            
+        </main>
+    );
+}
+
+function App() {
+
+
+  return (
+      <main className="App">
+          <Register />
+          
+      </main>
+  );
+}
+
+export default App;
---
+++ restaurant-app/src/Login.js
@@ -0,0 +1,54 @@
+import { useRef, useState, useEffect } from 'react';
+
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        useRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+
+    return (
+        <div>
+            <p ref={errRef} className={errMsg ? "errmsg" :
+            "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    ref={userRef}
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+                <button>Sign In</button> 
+            </form>
+        </div>
+    )
+}
+
+export default Login;
